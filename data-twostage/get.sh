4627aab572187e19c63f57ab94c157f5a7ec0f74
Milan Straka <milan@strakovi.com>
2026-07-03 14:56:42 +0200

    Add scripts for downloading CorefUD 1.4 data.

---
+++ data-twostage/get.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+set -e
+
+# Download the gold train and dev, and zero-predicted dev and test data.
+for conf in \
+  train:https://ufal.mff.cuni.cz/~mnovak/files/crac26/unc-gold-train.zip \
+  minidev.gold:https://ufal.mff.cuni.cz/~mnovak/files/crac26/unc-gold-minidev.zip \
+  minidev:https://ufal.mff.cuni.cz/~mnovak/files/crac26/unc-input_zeropred-minidev.zip \
+  minitest:https://ufal.mff.cuni.cz/~mnovak/files/crac26/unc-input_zeropred-minitest.zip
+do
+  split=${conf%%:*}
+  url=${conf#*:}
+
+  mkdir download
+  wget $url -O download/$split.zip
+  unzip -d download download/$split.zip
+  for f in download/*.conllu; do
+    lang=$(basename $f)
+    lang=${lang%%-*}
+    mkdir -p $lang
+    mv $f $lang/$lang-corefud-$split.conllu
+  done
+  rm download/$split.zip download/*.json
+  rmdir download   # Fails if not empty
+done
+
+echo All done
